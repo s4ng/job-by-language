4310dc3c7f42c1d2329541c45958198cd9daa710
s4ng <zalcls951201@gmail.com>
2022-06-26 23:05:43 +0900

    init commit

---
+++ src/main/kotlin/com/s4ng/stackcardgenerator/StackcardgeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.s4ng.stackcardgenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StackcardgeneratorApplication
+
+fun main(args: Array<String>) {
+    runApplication<StackcardgeneratorApplication>(*args)
+}
