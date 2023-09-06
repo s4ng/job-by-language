5d7cb80b8af65470d72c80a1d120f03c01201768
Lee Sang-geun <zalcls951201@gmail.com>
2023-09-06 16:59:42 +0900

    Update MainService.kt

--- src/main/kotlin/com/s4ng/jobbylanguage/service/MainService.kt
+++ src/main/kotlin/com/s4ng/jobbylanguage/service/MainService.kt
@@ -2,7 +2,6 @@ package com.s4ng.jobbylanguage.service
 
 import com.s4ng.jobbylanguage.model.dto.*
 import com.s4ng.jobbylanguage.model.entity.JobOpeningEntity
-import com.s4ng.jobbylanguage.model.entity.StackEntity
 import com.s4ng.jobbylanguage.model.enum.StackCategory
 import com.s4ng.jobbylanguage.repository.JobOpeningRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -35,11 +34,11 @@ class MainService(@Autowired val jobOpeningRepository: JobOpeningRepository) {
 
         return StatisticsDataDto(
                 languageRank = this.createRankDto(languageEntities, true),
-                languageFlow = createFlowDto(languageEntities),
+                languageFlow = createFlowDto(languageEntities, false),
                 frameworkRank = this.createRankDto(frameworkEntities, true),
-                frameworkFlow = createFlowDto(frameworkEntities),
+                frameworkFlow = createFlowDto(frameworkEntities, true),
                 databaseRank = this.createRankDto(databaseEntities, true),
-                databaseFlow = createFlowDto(databaseEntities),
+                databaseFlow = createFlowDto(databaseEntities, false),
                 lowLanguageRank = this.createRankDto(languageEntities, false),
                 lowFrameworkRank = this.createRankDto(frameworkEntities, false)
         )
@@ -61,20 +60,25 @@ class MainService(@Autowired val jobOpeningRepository: JobOpeningRepository) {
         return RankDataDto(names, values)
     }
 
-    fun createFlowDto(jobOpenings: List<JobOpeningEntity>): FlowDataDto {
+    fun createFlowDto(jobOpenings: List<JobOpeningEntity>, isFramework: Boolean): FlowDataDto {
 
         val sorts: List<SimpleJobOpening> = getSortedSimpleJobOpenings(jobOpenings, true)
+        val stackSize = if (isFramework) 8 else 6
 
         val top7StackNames: MutableList<String> = mutableListOf()
         for (i: Int in 0..6) {
             top7StackNames += sorts[i].name
         }
+        if (isFramework) {
+            top7StackNames += "springboot"
+            top7StackNames += "reactjs"
+        }
 
         val isMonth: Boolean = jobOpenings[0].createdTime.isBefore(ZonedDateTime.now().minusDays(91L))
 
-        val flowData: MutableList<SingleFlowData> = mutableListOf()
+        var flowData: MutableList<SingleFlowData> = mutableListOf()
         val labels: MutableList<Int> = mutableListOf()
-        for (i: Int in 0..6) {
+        for (i: Int in 0..stackSize) {
             val values: MutableList<Int> = mutableListOf()
             var now: Int = -1
             for (jobOpening: JobOpeningEntity in jobOpenings) {
@@ -99,7 +103,44 @@ class MainService(@Autowired val jobOpeningRepository: JobOpeningRepository) {
                     }
                 }
             }
-            flowData += SingleFlowData(top7StackNames[i], values, backgroundColor = colorList[i], borderColor = colorList[i])
+            flowData += SingleFlowData(top7StackNames[i], values, null, null)
+        }
+
+        if (isFramework) {
+            var springIndex: Int? = null;
+            var springBootIndex: Int? = null;
+            var reactIndex: Int? = null;
+            var reactjsIndex: Int? = null;
+            for (j: Int in 0..8) {
+                if (top7StackNames[j] == "spring") {
+                    springIndex = j;
+                }
+                if (top7StackNames[j] == "springboot") {
+                    springBootIndex = j;
+                }
+                if (top7StackNames[j] == "react") {
+                    reactIndex = j;
+                }
+                if (top7StackNames[j] == "reactjs") {
+                    reactjsIndex = j;
+                }
+            }
+            for (j: Int in 0 until labels.size) {
+                if (springIndex != null && springBootIndex != null) {
+                    flowData[springIndex].data[j] += flowData[springBootIndex].data[j];
+                }
+                if (reactIndex != null && reactjsIndex != null) {
+                    flowData[reactIndex].data[j] += flowData[reactjsIndex].data[j];
+                }
+            }
+        }
+
+
+        if (flowData.size > 7) {
+            flowData = flowData.subList(0, 7);
+        }
+        for (i: Int in 0..6) {
+            flowData[i].setColor(colorList[i])
         }
 
         return FlowDataDto(flowData, labels, isMonth)
@@ -108,15 +149,25 @@ class MainService(@Autowired val jobOpeningRepository: JobOpeningRepository) {
     fun getSortedSimpleJobOpenings(jobOpenings: List<JobOpeningEntity>, isTop: Boolean): List<SimpleJobOpening> {
 
         val aMonthAgo: ZonedDateTime = ZonedDateTime.now().minusMonths(1L)
-        val jobMap = HashMap<StackEntity, SimpleJobOpening>()
+        val jobMap = HashMap<String, SimpleJobOpening>()
         for (job: JobOpeningEntity in jobOpenings) {
             if (job.createdTime.isAfter(aMonthAgo)) {
-                if (!jobMap.containsKey(job.stack)) {
-                    jobMap[job.stack] = SimpleJobOpening(job.stack.name, job.value, 1, job.createdTime)
+                if (!jobMap.containsKey(job.stack.name)) {
+                    jobMap[job.stack.name] = SimpleJobOpening(job.stack.name, job.value, 1, job.createdTime)
                 } else {
-                    jobMap[job.stack]?.addValue(job.value)
+                    jobMap[job.stack.name]?.addValue(job.value)
                 }
             }
+        }
+
+        if (jobMap.containsKey("spring") && jobMap.containsKey("springboot")) {
+            jobMap["spring"]!!.addOnlyValue(jobMap["springboot"]!!.value)
+            jobMap.remove("springboot")
+        }
+
+        if (jobMap.containsKey("react") && jobMap.containsKey("reactjs")) {
+            jobMap["react"]!!.addOnlyValue(jobMap["reactjs"]!!.value)
+            jobMap.remove("reactjs")
         }
 
         val sorts: List<SimpleJobOpening>
